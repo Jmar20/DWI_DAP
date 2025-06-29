c9e0b3676805145885e8792886dea876ff1a9060
Jesús Alonso Romero <u22238783@utp.edu.pe>
2025-06-29 18:14:25 -0500

    Pequeña Config

--- src/main/java/org/utp/pydwi/CorsConfig.java
+++ src/main/java/org/utp/pydwi/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://victorious-coast-0693bca10.1.azurestaticapps.net")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
